68990223f17244d4c34c25f30b814b1714859c6f
HenrikTk <henrik.ruan4@gmail.com>
2020-07-26 12:28:06 -0300

    26/07/2020
    26/07/2020

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,20 +1,35 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody Player;
 
+    public float ForcaFrente = 1000f;
+    public float ForcaLados = 500f;
+    public float ForcaPulo = 100f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        Player.AddForce(0, 0, 200 * Time.deltaTime);
+        Player.AddForce(0, 0, ForcaFrente * Time.deltaTime);
+
+        if (Input.GetKey("a"))
+        {
+            Player.AddForce(-ForcaLados * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("d"))
+        {
+            Player.AddForce(ForcaLados * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("space"))
+        {
+            Player.AddForce(0, ForcaLados * Time.deltaTime, 0);
+        }
     }
 }
