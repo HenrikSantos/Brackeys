609f8238271bb8e655294a8059f0bd87d7847087
HenrikTk <henrik.ruan4@gmail.com>
2020-07-26 11:26:48 -0300

    Update PlayerMovement.cs

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public Rigidbody Player;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,6 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Player.AddForce(0, 0, 200 * Time.deltaTime);
     }
 }
